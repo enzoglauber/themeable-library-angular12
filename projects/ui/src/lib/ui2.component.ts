7e763a093c913b5da45261d9cf4ba4be789255c9
Enzo Glauber de Lima Almeida <enzo.almeida@bradesco.com.br>
2021-11-22 08:54:44 -0300

    save

---
+++ projects/ui/src/lib/ui2.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'ui-ui2',
+  template: `
+    <p>
+      ui2 works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class Ui2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ projects/ui/src/lib/ui2.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Ui2Component } from './ui2.component';
+
+
+
+@NgModule({
+  declarations: [
+    Ui2Component
+  ],
+  imports: [
+  ],
+  exports: [
+    Ui2Component
+  ]
+})
+export class Ui2Module { }
